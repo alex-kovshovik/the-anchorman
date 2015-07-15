ab9864d8ca5d554a4d99054546ad7f08a403baf0
Aliaksandr Kaushovik <alexk@Alex-Ks-Mac.local>
2015-07-15 14:31:33 -0600

    Fixed production deployment script.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server '192.241.209.22', user: 'app', roles: %w{app db web}, my_property: :my_value
+server '192.241.209.22:3422', user: 'app', roles: %w{app db web}, my_property: :my_value
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
 
