d54f515fcd4007e41e8539538d2925c68c0ff1f1
Alex Kovshovik <Alex.Kovshovik@plansource.com>
2015-08-12 16:27:18 -0600

    Improved home page content.

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :repo_url, 'https://github.com/aspushkinus/take_dat_shit.git'
 set :deploy_to, "/srv/www/#{fetch(:application)}"
 
 # Default value for :linked_files is []
-set :linked_files, fetch(:linked_files, []).push('config/database.yml')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 
 # Default value for linked_dirs is []
 # set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
