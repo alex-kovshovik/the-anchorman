3bbba1563f85af2634ab8bbebcf0188632453503
Alex Kovshovik <Alex.Kovshovik@plansource.com>
2015-08-19 14:46:08 -0600

    Implemented long polling for the toilet status - websockets and notifications are coming next!

--- app/controllers/toilets_controller.rb
+++ app/controllers/toilets_controller.rb
@@ -2,6 +2,8 @@ class ToiletsController < ApplicationController
 
   skip_before_action :verify_authenticity_token
 
+  before_action :find_toilet, only: [:show, :update]
+
   def index
     toilets = Toilet.all
 
@@ -10,18 +12,17 @@ class ToiletsController < ApplicationController
     end
   end
 
-  def update
-    toilet = Toilet.find_by(id: params[:id])
-
-    if toilet.nil?
-      head :not_found
-      return
+  def show
+    respond_to do |format|
+      format.json { render json: @toilet, status: :ok }
     end
+  end
 
-    if toilet.update(toilet_params)
-      render json: toilet, status: :ok
+  def update
+    if @toilet.update(toilet_params)
+      render json: @toilet, status: :ok
     else
-      render json: toilet.errors, status: :unprocessable_entity
+      render json: @toilet.errors, status: :unprocessable_entity
     end
   end
 
@@ -29,6 +30,17 @@ class ToiletsController < ApplicationController
 
   def toilet_params
     params.require(:toilet).permit(:state)
+  end
+
+  def find_toilet
+    @toilet = Toilet.find_by(id: params[:id])
+
+    if @toilet.nil?
+      head :not_found
+      false
+    end
+
+    true
   end
 
 end
