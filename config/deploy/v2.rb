51a3a041d802e82c8858afa383ff6f70fa1ebc88
Alex Kovshovik <Alex.Kovshovik@plansource.com>
2015-10-02 09:06:05 -0600

    Experiment with inline assets.

--- config/deploy/v2.rb
+++ config/deploy/v2.rb
@@ -1,7 +1,7 @@
 server '192.241.209.22:3422', user: 'app', roles: %w{app db web}, my_property: :my_value
 set :rvm_ruby_version, '2.2.3'
 
-set :application, 'tds'
-set :branch, 'master'
+set :application, 'anchorman'
+set :branch, 'v2'
 set :deploy_to, "/srv/www/#{fetch(:application)}"
 set :rails_env, "production"
